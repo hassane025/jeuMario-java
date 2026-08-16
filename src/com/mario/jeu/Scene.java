9212521c09a2f0dc40288d83ac5c5d0573fb47df
hassane025 <traorehassane025@gmail.com>
2026-08-16 19:30:07 +0100

    effet de deplacement sur l axe x en bougeant le imgfond

--- src/com/mario/jeu/Scene.java
+++ src/com/mario/jeu/Scene.java
@@ -12,24 +12,46 @@ public class Scene extends JPanel {
     private Image imgMario;
 
     private int xFond1;
+    private int dx;
 
     public Scene(){
         super();
 
         this.xFond1 = -50;
+        this.dx = 0;
+
         icoFond = new ImageIcon(getClass().getResource("/images/fondEcran.png"));
         this.imgFond1 = this.icoFond.getImage();
         icoMario = new ImageIcon(getClass().getResource("/images/marioMarcheDroite.png"));
         this.imgMario = this.icoMario.getImage();
 
+        this.setFocusable(true);
+        this.requestFocusInWindow(true);
+        this.addKeyListener(new Clavier());
+
         Thread chronoEcran = new Thread(new Chrono());
         chronoEcran.start();
+    }
+
+    public void deplacementFond(){
+        this.xFond1 = this.xFond1 - this.dx;
+    }
+
+    /***Getters**/
+    public int getDx() {
+        return dx;
+    }
+
+    /***setters***/
+    public void setDx(int dx) {
+        this.dx = dx;
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics g2 = (Graphics2D)g;
 
+        this.deplacementFond();
         g2.drawImage(this.imgFond1, this.xFond1, 0, null);
         g2.drawImage(imgMario, 300, 245, null);
     }
